d13e8829f859a96fdf840302312647eded97730b
chinnawat ong-art <chinnawat.ongart@gmail.com>
2020-05-31 03:26:11 +0700

    implement update student

--- schools/serializers.py
+++ schools/serializers.py
@@ -9,10 +9,10 @@ class SchoolSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "max_student")
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class StudentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
-        fields = ("student_id", "first_name", "last_name", "school")
+        fields = ("url", "student_id", "first_name", "last_name", "school")
 
     def create(self, validated_data):
         number_of_students = Student.objects.filter(
@@ -22,3 +22,19 @@ class StudentSerializer(serializers.ModelSerializer):
         if number_of_students >= limit_number_of_student:
             raise serializers.ValidationError("this school is full. please join to another school.")
         return Student.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        number_of_students = Student.objects.filter(
+            school=validated_data.get('school')).count()
+        limit_number_of_student = School.objects.get(
+            pk=validated_data.get('school').id).max_student
+        if number_of_students >= limit_number_of_student:
+            raise serializers.ValidationError("this school is full. please join to another school.")
+
+        instance.student_id = validated_data.get('student_id', instance.student_id)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.school = validated_data.get('school', instance.school)
+        instance.save()
+
+        return instance
