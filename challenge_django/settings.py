6e357f96d5bf47f94489d5b58d7d9395baa36e61
chinnawat ong-art <chinnawat.ongart@gmail.com>
2020-05-31 19:57:44 +0700

    add gunicorn

--- challenge_django/settings.py
+++ challenge_django/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'challenge',
         'USER': 'djangoapp',
         'PASSWORD': 'test',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
